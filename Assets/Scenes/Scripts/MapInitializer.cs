fc3689619b2bbc328fe4fbd10c44dab6c4cc1f16
Dmitriy Korotkov <nupellot@yahoo.com>
2022-05-07 22:05:30 +0300

    Версия карты 1.1

--- Assets/Scenes/Scripts/MapInitializer.cs
+++ Assets/Scenes/Scripts/MapInitializer.cs
@@ -16,7 +16,7 @@ public class MapInitializer : MonoBehaviour {
     {
         RoadSize = Roadway.GetComponent<Renderer>().bounds.size.z;  // Ширина дороги.
         GrassSurfaceSize = GrassSurface.GetComponent<Renderer>().bounds.size.z;  // Ширина травяного покрова.
-        GenerateMap(10);
+        GenerateMap(30);
 
         for (int i = 0; i < Map.Count; i++) {
           Debug.Log(Map[i]);
@@ -25,13 +25,20 @@ public class MapInitializer : MonoBehaviour {
         Debug.Log(Map.Count);
         Debug.Log(System.String.Join(", ", Map));
 
+        float CurrentZ = 0;  // Переменная, отслеживающая крайнюю позицию карты.
         for (int i = 0; i < Map.Count; i++) {
             if (Map[i] == 1) {
                 Debug.Log("Roadway at " + i);
-                Spawn(Roadway, new Vector3(0, 0, RoadSize * i), i);
+                CurrentZ += RoadSize;
+                Instantiate(Roadway);
+                Roadway.transform.position = new Vector3(0, 0, CurrentZ);
+                // Spawn(Roadway, new Vector3(0, 0, RoadSize * i), i);
             } else if (Map[i] == 0) {
                 Debug.Log("GrassSurface at " + i);
-                Spawn(GrassSurface, new Vector3(0, 0, GrassSurfaceSize * i), i);
+                CurrentZ += GrassSurfaceSize;
+                Instantiate(GrassSurface);
+                GrassSurface.transform.position = new Vector3(0, 0, CurrentZ);
+                // Spawn(GrassSurface, new Vector3(0, 0, GrassSurfaceSize * i), i);
             } else {
                 Debug.Log("Wrong map element " + i + ": " + Map[i]);
             }
@@ -54,14 +61,14 @@ public class MapInitializer : MonoBehaviour {
     }
 
 
-    void Spawn(GameObject Object, Vector3 Coords, int number) {
-        Instantiate(Object);
-        Vector3 Size = Object.GetComponent<Renderer>().bounds.size;
-        // Позиционируем объект, сдвигая на половину его длины, чтобы его начало оказалось в левом нижнем углу.
-        // Object.transform.position = new Vector3(Coords.x - Size.x / 2, Coords.y - Size.y / 2, Coords.z - Size.z / 2);
-        Object.transform.position = new Vector3(Coords.x, Coords.y, Coords.z);
-        Debug.Log(number + " " + " Positioned to " + Coords.x + " " + Coords.z);
-        Debug.Log("Spawn got " + number);
-        Object.name = "Line " + number;
-    }
+    // void Spawn(GameObject Object, Vector3 Coords, int number) {
+    //     Instantiate(Object);
+    //     Vector3 Size = Object.GetComponent<Renderer>().bounds.size;
+    //     // Позиционируем объект, сдвигая на половину его длины, чтобы его начало оказалось в левом нижнем углу.
+    //     // Object.transform.position = new Vector3(Coords.x - Size.x / 2, Coords.y - Size.y / 2, Coords.z - Size.z / 2);
+    //     Object.transform.position = new Vector3(Coords.x, Coords.y, Coords.z);
+    //     Debug.Log(number + " " + " Positioned to " + Coords.x + " " + Coords.z);
+    //     Debug.Log("Spawn got " + number);
+    //     Object.name = "Line " + number;
+    // }
 }
