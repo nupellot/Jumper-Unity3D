f27a47885990a031d94d60fb59d5a7f96e28bb7a
Dmitriy Korotkov <nupellot@yahoo.com>
2022-06-03 17:53:12 +0300

    Новая генерация карты

--- Assets/Scenes/Scripts/MapInitializer.cs
+++ Assets/Scenes/Scripts/MapInitializer.cs
@@ -13,7 +13,8 @@ public class MapInitializer : MonoBehaviour {
     public float CarSpeed = 10;
     public float MinCarSpawnTime = 1;
     public float MaxCarSpawnTime = 10;
-    public int InitialFieldLength = 100;
+    public int InitialFieldLength = 10;
+    public int GenerationGap = 10;
     // public float LineSize = 30;
 
     private List<GameObject> Roads = new List<GameObject>();
@@ -22,7 +23,9 @@ public class MapInitializer : MonoBehaviour {
     private GameObject Player;
     private float RoadSize;
     private float GrassSurfaceSize;
-    private List<int> Map = new List<int>() {0, 0, 0, 0, 0};  // Карта игрового поля.
+    private float LineSize;
+    private int CurrentFieldLength = 0;
+    private List<int> Map = new List<int>();  // Карта игрового поля.
 
 
     void Start()
@@ -31,28 +34,30 @@ public class MapInitializer : MonoBehaviour {
         // Создаём карту.
         RoadSize = Roadway.GetComponent<Renderer>().bounds.size.z;  // Ширина дороги.
         GrassSurfaceSize = GrassSurface.GetComponent<Renderer>().bounds.size.z;  // Ширина травяного покрова.
-        GenerateMap(InitialFieldLength);  // Получаем двоичную карту.
-        // Record.text;
-
-        // Создаём само полотно.
-        float CurrentZ = 0;  // Переменная, отслеживающая крайнюю позицию карты.
-        for (int i = 0; i < Map.Count; i++) {
-            if (Map[i] == 1) {  // Дорога.
-                CurrentZ += RoadSize;
-                // Создаём новый элемент дороги и сразу же засовываем его в массив.
-                Roads.Add(Instantiate(Roadway, new Vector3(0, 0, CurrentZ), Quaternion.identity));
-                Roads.Last().name = "Line" + i;  // Задаём имя, отоборажающееся в инспекторе.
-                Debug.Log("Roadway at " + i);
-            } else if (Map[i] == 0) {  // Травяной покров.
-                CurrentZ += GrassSurfaceSize;
-                Grasses.Add(Instantiate(GrassSurface, new Vector3(0, 0, CurrentZ), Quaternion.identity));
-                Grasses.Last().name = "Line" + i;
-                Debug.Log("GrassSurface at " + i);
-            } else {
-                Debug.Log("Wrong map element " + i + ": " + Map[i]);
-            }
-        }
-
+        LineSize = Roadway.GetComponent<Renderer>().bounds.size.z;
+        // GenerateMap(InitialFieldLength);  // Получаем двоичную карту.
+        // // Record.text;
+        //
+        CreateMap();
+                // // Создаём само полотно.
+        // float CurrentZ = 0;  // Переменная, отслеживающая крайнюю позицию карты.
+        // for (int i = 0; i < Map.Count; i++) {
+        //     if (Map[i] == 1) {  // Дорога.
+        //         CurrentZ += RoadSize;
+        //         // Создаём новый элемент дороги и сразу же засовываем его в массив.
+                // Roads.Add(Instantiate(Roadway, new Vector3(0, 0, CurrentZ), Quaternion.identity));
+                // Roads.Last().name = "Line" + i;  // Задаём имя, отоборажающееся в инспекторе.
+        //         Debug.Log("Roadway at " + i);
+        //     } else if (Map[i] == 0) {  // Травяной покров.
+                // CurrentZ += GrassSurfaceSize;
+                // Grasses.Add(Instantiate(GrassSurface, new Vector3(0, 0, CurrentZ), Quaternion.identity));
+                // Grasses.Last().name = "Line" + i;
+        //         Debug.Log("GrassSurface at " + i);
+        //     } else {
+        //         Debug.Log("Wrong map element " + i + ": " + Map[i]);
+        //     }
+        // }
+        //
         // Делаем барьеры для автомобилей.
         GameObject LeftCarBarrier = Instantiate(CarBarrier, new Vector3(0 - GrassSurface.GetComponent<Renderer>().bounds.size.x / 2, 0, 0 + CarBarrier.GetComponent<Renderer>().bounds.size.x / 2), Quaternion.Euler(0, 0, 90));
         GameObject RightCarBarrier = Instantiate(CarBarrier, new Vector3(0 + GrassSurface.GetComponent<Renderer>().bounds.size.x / 2, 0, 0 + CarBarrier.GetComponent<Renderer>().bounds.size.x / 2), Quaternion.Euler(0, 0, 90));
@@ -66,26 +71,51 @@ public class MapInitializer : MonoBehaviour {
         //     Cars.Add(Instantiate(Car, Roads[i].transform.position - Roads[i].GetComponent<Renderer>().bounds.size / 2 + Car.GetComponent<Renderer>().bounds.size / 2, Quaternion.identity));
         //     Cars.Last().name = "Car" + i;
         // }
-        StartCarSpawn();
+        // StartCarSpawn();
     }
 
     // Update is called once per frame
     void Update() {
+        EnlargeMap();
         CleanDeletedCars();
         SetCarsSpeed();
         UpdateRecord();
-        EnlargeMap();
+    }
+
+    void CreateMap() {
+        for (int i = 0; i < Map.Count; i++) {
+            Grasses.Add(Instantiate(GrassSurface, new Vector3(0, 0, LineSize * CurrentFieldLength), Quaternion.identity));
+            Grasses.Last().name = "Line" + CurrentFieldLength;
+            CurrentFieldLength++;
+        }
     }
 
     void EnlargeMap() {
-        int CurrentZPosition = PlayerPrefs.GetInt("CurrentZPosition");
+        if (Map.Count - PlayerPrefs.GetInt("CurrentZPosition") < GenerationGap) {
+            System.Random random = new System.Random();
+            Map.Add(random.Next(0, 2));
 
+            if (Map[Map.Count - 1] == 1) {  // Дорога.
+                Roads.Add(Instantiate(Roadway, new Vector3(0, 0, LineSize * CurrentFieldLength), Quaternion.identity));
+                Roads.Last().name = "Line" + CurrentFieldLength;  // Задаём имя, отоборажающееся в инспекторе.
+                StartCarSpawner(Roads[Roads.Count - 1]);
+            } else
+            if (Map[Map.Count - 1] == 0) {  // Трава.
+                Grasses.Add(Instantiate(GrassSurface, new Vector3(0, 0, LineSize * CurrentFieldLength), Quaternion.identity));
+                Grasses.Last().name = "Line" + CurrentFieldLength;
+            }
+            CurrentFieldLength++;
+        }
     }
 
     void StartCarSpawn() {
         foreach (GameObject Road in Roads) {
             StartCoroutine(CarCoroutine(Road));
         }
+    }
+
+    void StartCarSpawner(GameObject Road) {
+        StartCoroutine(CarCoroutine(Road));
     }
 
     IEnumerator CarCoroutine(GameObject Road) {
@@ -139,29 +169,7 @@ public class MapInitializer : MonoBehaviour {
             Debug.Log("Рекорд сброшен");
         }
     }
-    // void OnCollisionEnter(Collision col) {
-    //     Debug.Log("Collision");
-    //     if (this.gameObject.CompareTag("Car") && col.gameObject.CompareTag("Barrier")) {
-    //         Destroy(this.gameObject);
-    //     }
-    // }
-
-    // private void OnTriggerEnter(Collider other) {
-    //     Debug.Log("Collision");
-    //     if (this.CompareTag("Car") && other.CompareTag("Barrier")) {
-    //         Destroy(this.gameObject);
-    //     }
-    // }
 
 
-    // void Spawn(GameObject Object, Vector3 Coords, int number) {
-    //     Instantiate(Object);
-    //     Vector3 Size = Object.GetComponent<Renderer>().bounds.size;
-    //     // Позиционируем объект, сдвигая на половину его длины, чтобы его начало оказалось в левом нижнем углу.
-    //     // Object.transform.position = new Vector3(Coords.x - Size.x / 2, Coords.y - Size.y / 2, Coords.z - Size.z / 2);
-    //     Object.transform.position = new Vector3(Coords.x, Coords.y, Coords.z);
-    //     Debug.Log(number + " " + " Positioned to " + Coords.x + " " + Coords.z);
-    //     Debug.Log("Spawn got " + number);
-    //     Object.name = "Line " + number;
-    // }
+
 }
